e57a6363823b7c7f8ca424f3332488979a6781d0
Rohit Sharma <rohit2sharma95@gmail.com>
2020-10-10 22:52:48 +0530

    Updates to fix the issues in deployments

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/heroku-app'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/heroku-app/index.html'));
 });
+app.listen(process.env.PORT || 8080);
